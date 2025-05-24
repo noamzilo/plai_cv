5a5cc659302425527a1c456f971084128dc52fa5
noam_wsl <noamzilo@gmail.com>
2025-05-23 21:23:21 -0500

    averaging over video to calibrate

--- src/calibration/calibration_main.py
+++ src/calibration/calibration_main.py
@@ -11,7 +11,7 @@ def main():
 	video_path = raw_data_path / video_name 
 	assert video_path.is_file()
 	video_averager = VideoAverager(video_path)
-	video_averager.average_frames(interval=10)
+	video_averager.average_frames(start_frame=0, interval=10, end_frame=2000)
 	
 	video_calculated_path = calculated_data_path /f"{video_name}"
 	os.makedirs(video_calculated_path, exist_ok=True)
