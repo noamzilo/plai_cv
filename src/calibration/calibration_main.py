d40dc02b719be96bae35ab96cf49ceb7f40a4785
noam_wsl <noamzilo@gmail.com>
2025-05-23 23:23:37 -0500

    attempts to calculate camera intrinsics

--- src/calibration/calibration_main.py
+++ src/calibration/calibration_main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 
 from calibration.VideoAverager import VideoAverager
+from calibration.PitchCorners import PitchCorners
 
 
 def main():
@@ -11,12 +12,16 @@ def main():
 	video_path = raw_data_path / video_name 
 	assert video_path.is_file()
 	video_averager = VideoAverager(video_path)
-	video_averager.average_frames(start_frame=0, interval=10, end_frame=2000)
+	video_averager.average_frames(start_frame=0, interval=10, end_frame=20000)
 	
 	video_calculated_path = calculated_data_path /f"{video_name}"
 	os.makedirs(video_calculated_path, exist_ok=True)
-	average_frame_path = video_calculated_path / f"average_frame.bmp"
+	average_frame_name = f"average_frame.bmp"
+	average_frame_path = video_calculated_path / average_frame_name 
 	video_averager.save_average_frame(calculated_data_path / average_frame_path)
+
+	pitch_corners = PitchCorners(image_path=average_frame_path)
+	pitch_corners.calculate_corners()
 
 
 if __name__ == "__main__":
