8e8bb592dc458a7bb9f80d290ef2d2b86011453f
noam_wsl <noamzilo@gmail.com>
2025-05-26 14:42:02 -0500

    calibration pnp but something is wrong

--- src/calibration/pitch_corners_const.py
+++ src/calibration/pitch_corners_const.py
@@ -6,4 +6,7 @@ net_right_bottom = np.array([2955, 751])
 net_center_bottom = np.array([1906, 731])
 far_left_corner = np.array([1372, 485])
 far_right_corner = np.array([2451, 510])
-close_white_line_center = np.array([1881, 1635])
+close_white_line_center = np.array([1863, 1622])
+
+image_width = 3840
+image_height = 2160
---
+++ src/calibration/pitch_keypoints.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# units are meters
+far_left_corner_3d = np.array([0.0, 0.0, 0.0])
+far_right_corner_3d = np.array([10.0, 0.0, 0.0])
+net_left_bottom_3d = np.array([0., 10., 0])
+net_right_bottom_3d = np.array([10., 10., 0])
+net_center_bottom_3d = np.array([5., 10., 0])
+close_white_line_center_3d = np.array([4.95, 16.95, 0])
