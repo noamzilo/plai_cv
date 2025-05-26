7166f7f0819cd09a6aa0cf53494b51e497ab17ed
noam_wsl <noamzilo@gmail.com>
2025-05-26 13:11:47 -0500

    starting with pnp calibration

---
+++ src/calibration/pitch_corners_const.py
@@ -0,0 +1,9 @@
+# pitch corners in game 1, hard coded. This will later be the output of a model
+import numpy as np
+
+net_left_bottom = np.array([840, 705])
+net_right_bottom = np.array([2955, 751])
+net_center_bottom = np.array([1906, 731])
+far_left_corner = np.array([1372, 485])
+far_right_corner = np.array([2451, 510])
+close_white_line_center = np.array([1881, 1635])
