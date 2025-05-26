d2ea921c15248d4a89e44291818fa49ea74f6e4c
noam_wsl <noamzilo@gmail.com>
2025-05-26 15:40:37 -0500

    got 3d info into the pnp but the measurements must not be correct because it fails badly

--- src/calibration/pitch_corners_const.py
+++ src/calibration/pitch_corners_const.py
@@ -6,7 +6,12 @@ net_right_bottom = np.array([2955, 751])
 net_center_bottom = np.array([1906, 731])
 far_left_corner = np.array([1372, 485])
 far_right_corner = np.array([2451, 510])
-close_white_line_center = np.array([1863, 1622])
+close_white_line_center_left_far = np.array([1863, 1624])
+close_white_line_center_left_close = np.array([1862, 1647])
+close_white_line_center_right_far = np.array([1900, 1624])
+close_white_line_center_right_close = np.array([1900, 1649])
+net_left_top  = np.array([825, 507])
+net_right_top = np.array([2980, 555])
 
 image_width = 3840
 image_height = 2160
---
+++ src/calibration/pitch_keypoints_3d.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# units are meters
+far_left_corner_3d = np.array([0.0, 0.0, 0.0])
+far_right_corner_3d = np.array([10.0, 0.0, 0.0])
+net_left_bottom_3d = np.array([0., 10., 0])
+net_right_bottom_3d = np.array([10., 10., 0])
+net_center_bottom_3d = np.array([5., 10., 0])
+close_white_line_center_left_far_3d = np.array([4.975, 16.95, 0])
+close_white_line_center_left_close_3d= np.array([4.975, 17.0, 0])
+close_white_line_center_right_far_3d = np.array([5.025, 16.95, 0])
+close_white_line_center_right_close_3d = np.array([5.025, 17.0, 0])
+net_left_top_3d  = np.array([0.0, 10.0, 0.88])
+net_right_top_3d = np.array([10.0, 10.0, 0.88])
